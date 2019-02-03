af48604872858042ed4a0f5c80e8d057adb03c17
Maurice Barnes <mauriceb2197@gmail.com>
2019-02-03 10:14:54 -0500

    Last Push

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const bodyparser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 3001;
 os = require('os');
 fs = require('fs');
 var Busboy = require('busboy');
+var bb = require('express-busboy');
+bb.extend(app, {
+
+});
 app.use(cors());
 
 app.listen(port);
@@ -14,7 +17,7 @@ app.listen(port);
 app.post('/', (req,res) => {
     console.log("hi")
     
-    var busboy = new Busboy({ headers: req.headers});
+    /*var busboy = new Busboy({ headers: req.headers});
     busboy.on('image', (fieldname, image, filename, encoding, mimetype) => {
         var saveTo = path.join(os.tmpDir(), path.basename(fieldname));
         file.pipe(fs.createWriteStream(saveTo));
@@ -23,12 +26,14 @@ app.post('/', (req,res) => {
     busboy.on('finish', () => {
         res.writeHead(200, {'Connection' : 'close'});
         res.end("That's all folks");
-    });
+    });*/
 
-    return req.pipe(busboy);
-    //var image = req.body.image;
-    //var imagepath = req.body.imagepath;
+    //return req.pipe(busboy);
+    var image = req.body.image;
+    var imagepath = req.body.imagepath;
+
     
+    res.end('It worked')
     
 
 });
