eb7d44d3684f9d2262e891c29054e0d69b8fb61f
Svilla27 <43285453+Svilla27@users.noreply.github.com>
2019-02-02 22:39:45 -0500

    Add files via upload

---
+++ nn.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+import pickle
+
+
+X = pickle.load(open(r"C:\Users\stevo\OneDrive\Desktop\Datasets", "rb"))
+y = pickle.load(open(r"C:\Users\stevo\OneDrive\Desktop\Datasets", "rb"))
+
+X = x/255.0
+
+model = Sequential()
+
+model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)
