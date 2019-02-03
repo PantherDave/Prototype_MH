c8e910639d8d93e9ec8391560f1cda7c06d81f26
PantherDave <39570366+PantherDave@users.noreply.github.com>
2019-02-02 22:57:03 -0500

    Update nn.py

--- nn.py
+++ nn.py
@@ -17,7 +17,7 @@ model = Sequential()
 model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
+#train the network
 model.add(Conv2D(256, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
