ee456b21196918ffdf3f477ec21f604fc961689e
Maurice Barnes <mauriceb2197@gmail.com>
2019-02-05 18:04:55 -0500

    Got rid of the clutter

--- server.py
+++ server.py
@@ -4,14 +4,14 @@ from flask import request
 from flask import Flask
 from flask import Response
 from google.cloud import storage
-
+from flask import send_file, send_from_directory
 
 app = Flask(__name__)
 #ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 #UPLOAD_FOLDER = 'C:\\Users\\Maurice\\Documents\\GitHub\\Prototype_MH\\selfie-saver\\tmp'
-app.config['UPLOAD_FOLDER'] = 'C:\\Users\\Maurice\\Documents\\GitHub\\Prototype_MH\\selfie-saver\\tmp'
+app.config['UPLOAD_FOLDER'] = 'C:\\Users\\Maurice\\Documents\\GitHub\\Prototype_MH\\tmp\\'
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['POST','GET'])
 def file_upload():    
 
 
@@ -21,25 +21,15 @@ def file_upload():
     
     
     f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-    fpath = (os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+    #fpath = (os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
     
-    upload_blob('testbarnes2197', fpath, f.filename,)
-    return "It worked"
-
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+    #upload_blob('testbarnes2197', fpath, f.filename,)
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               f.filename, as_attachment=True)
 
 
-    blob.upload_from_filename(source_file_name)
-
-    print('File {} uploaded to {}.' .format(
-        destination_blob_name,
-        bucket_name
-    ))
-
+    
+    
 
 
 if __name__ == '__main__':
